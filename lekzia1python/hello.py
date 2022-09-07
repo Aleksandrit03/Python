d20fc69dbd1c69c8ab94c1bc8a08e93797b92407
«Aleksandr» <akhlopushin98@mail.ru>
2022-09-07 10:51:57 +0400

    Создаем папку для змеи

---
+++ lekzia1python/hello.py
@@ -0,0 +1,78 @@
+#print('hello world')
+#value = None
+#value = 3241
+#print(a)
+#print(b)
+#print (type(a)) # Тип переменной
+#print (type(b))
+#print (type(value))
+#print(s)
+#print (type(s)) # Вывод строки
+#d = "hello 'world" # Можно использовать двойные ковычки для присвоение 
+#print(d)
+#t = 'trum \'trum' # Можно использовать обратный слэш для написания символа, например как '
+#print(t)
+#t = 'trum \npum pum' # Так же работает перенос на новую строку
+#a = 123
+#b = 2.34
+#s = 'hello world' # Строка
+#print(a, "-", b, '-', s)
+#print('{} - {} - {}'.format(a, b, s)) # Используется для вывода
+#print(f'{a} - {b} - {s}')
+#print('{1} - {2} - {0}'.format(a, b, s))
+#t = True
+#print(t)
+#list = [1, 2, 'true']
+#print(list)
+
+#print('Введите число: ') # Для того чтоб преобразовать в нужный тип данных, необходимо перед input вставить его
+#a = int(input())
+#print('Введите число: ')
+#b = float(input())
+#print('Введенное числа: ', a, b)
+#print(f'{a}, {b}')
+
+# Арифметические операции
+#print(a, '+', b, '=', a+b)
+#a = 124
+#b = 58.5
+#c= a+b
+#print(c)
+#a = 17
+#b = 17
+#c= a/b # Деление вещественные числа
+#print(c)
+#c= a//b # Деление в целых числах
+#print(c)
+#c= a%b # Остаток от деления
+#print(c)
+#c= a**b # Возведение a в степень b
+#print(c)
+#a = 1.3234
+#b = 3
+#c = round(a * b) # С помощью РАУНД идет округление по мат правилам
+#print(c)
+#c = round(a * b, 2) # Можно указать сколько неоходимо цифр после запятой
+#print(c)
+#a = 3
+#a = a + 5
+#print(a) # Можно записать иначе
+#a += 5
+#print(a)
+#a *= 5
+#print(a)
+#a = int(input('a ='))
+#b = int(input('b = '))
+#if a > b:
+#    print(a)
+#elif b > a: 
+#    print(b)
+#else:
+#    print('Числа равны') 
+#list = [1,4,5,8,654]
+#for i in list:
+#    print(i**2)
+
+#for i in range(1, 20, 2):
+#    print(i)
+
