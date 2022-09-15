bcfcdd6a822d12189e2d36e58badf1020336c884
«Aleksandr» <akhlopushin98@mail.ru>
2022-09-15 20:03:11 +0400

    cddc

--- HomeWork4/Task1.py
+++ HomeWork4/Task1.py
@@ -1,8 +1,11 @@
 #Пользователь вводит число, Вам необходимо вывести число Пи с той точностью знаков после запятой, 
 #сколько указал пользователь(БЕЗ ИСПОЛЬЗОВАНИЯ МОДУЛЕЙ/БИБЛИОТЕК)
 n = int(input('Введите число: '))
+pi = float(open("ChisloPI.txt"))
 #c = round(, n)
+print(pi)
+exit()
 data = []
 with open("ChisloPI.txt") as n:
     for line in n:
-        data.append([float(x) for x in line.split()])
+        print()
