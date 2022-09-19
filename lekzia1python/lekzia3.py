c24ca853d2cc16c6202c19138496ecec2d42f04d
«Aleksandr» <akhlopushin98@mail.ru>
2022-09-19 22:35:34 +0400

    Добавлена задача

---
+++ lekzia1python/lekzia3.py
@@ -0,0 +1,30 @@
+def sum(x,y):  # Функция сложения 
+    return x + y
+f = sum # Можно присвоить функцию какому то аргументу
+
+def mult(x,y): # Функция умножения
+    return x * y
+
+g = mult
+def calc(op, a, b): # Функция с помощью которой мы можем передать функцию как аргумент (ор).
+    print(op(a,b))  # Значения "а" и "b" не должны быть идентичны тем что в функциях
+
+calc(mult, 4, 5)
+calc(sum, 4, 5)
+calc(g, 4, 5)
+calc(f, 4, 5)
+
+                                  # ЛЯМБДА
+summ = lambda x, y: x + y # Первую функцию можно переписать так
+print(summ(4, 6))
+calc(summ, 4, 7)
+calc(lambda x, y: x + y, 4, 11)
+
+                                  # СПИСОК
+spisok = [i for i in range(1, 21) if i%2 == 0]
+print(spisok)
+                                  # СПИСОК КОРТЕЖЕЙ
+def mult(x): 
+    return x**2
+spisok = [(i, mult(i)) for i in range(1, 21) if i%2 == 0]
+print(spisok)
