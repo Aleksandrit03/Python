df56e4966f3be28b748ca0bb2423a80a5d645172
«Aleksandr» <akhlopushin98@mail.ru>
2022-09-19 21:09:32 +0400

    Решена задача 4

--- HomeWork4/Task4.py
+++ HomeWork4/Task4.py
@@ -3,3 +3,29 @@
 
 #Пример:
 #- k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+import random
+flag = True
+num = int(input('Введите коэффециент: '))
+f = open('mnogochlen.txt','a')
+while flag:
+    rand = random.randint(0, 100)
+    if num == 0:
+        if rand == 0:
+            f.write('')
+        else:
+            f.write(f'{rand}')
+            num -= 1
+    elif num == -1:
+        f.write(' = 0\n')
+        flag = False
+    else:
+        if rand == 0:
+            f.write('')
+        else:
+            if num == 1:
+                f.write(f'{rand}x + ')
+                num -= 1
+            else:
+                f.write(f'{rand}x^{num} + ')
+                num -= 1
+f.close()
